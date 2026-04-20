b156fee30430ae5208dc361b03aa3647a55c5bcd
Somnath Chakraborty <somnathc352@gmail.com>
2026-04-21 01:04:48 +0530

    Crated project structure including all microservice

---
+++ api-gateway/src/test/java/in/somuxdev/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.somuxdev.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
